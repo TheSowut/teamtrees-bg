fecafbebe366c0832371ed343beadc33b063804b
Petar Gavazov <p.gavazov@protonmail.com>
2020-02-29 10:18:02 +0200

    Initial commit

---
+++ scripts/myScript.js
@@ -0,0 +1,34 @@
+var btnScroll = document.getElementById("scrollToTop");
+btnScroll.addEventListener("click", scrollToTop);
+window.addEventListener("scroll", scrollFunction);
+
+var copy = document.getElementById("copyright");
+var year = new Date().getFullYear();
+copy.innerHTML = "copyright" + " © " + year + " @pgavazov";
+
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+  // Функция предназначена да работи на Firefox, Chrome, Opera, IE
+  document.body.scrollTop = 0;
+  // Функция предназначена да работи на Safari
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
+    btnScroll.style.display = "block";
+    var leftPanel = document.getElementById("leftPanel");
+    leftPanel.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    var main = document.getElementsByTagName("main")[0];
+    main.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    var rightPanel = document.getElementById("rightPanel");
+    rightPanel.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  } else {
+    btnScroll.style.display = "none";
+    var leftPanel = document.getElementById("leftPanel");
+    leftPanel.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+    var main = document.getElementsByTagName("main")[0];
+    main.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+    var rightPanel = document.getElementById("rightPanel");
+    rightPanel.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+  }
+}
